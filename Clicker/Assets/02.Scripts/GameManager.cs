9ff56b39b9dac08a59412edfb0f1649a44d211f2
rymyung <rymyung@hanmail.net>
2019-05-19 21:24:17 +0900

    Update Scene & GameManager

---
+++ Clicker/Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+    public static long money;
+
+    private void Awake() {
+        gm = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        earnMoney();
+    }
+
+    public void earnMoney() {
+        if (Input.GetMouseButtonDown(0)) {
+            money += 1;
+            Debug.Log(money);
+        }
+    }
+}
